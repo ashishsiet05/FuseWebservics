20c74333aaa213393bad4615e097417ee8250da8
Isaac <you@example.com>
2021-03-16 22:10:41 -0600

    QueryLog for Hawtio added

--- projects/wscalculator/src/main/java/com/redhat/fuse/demos/ws/wssuma/SumProcessor.java
+++ projects/wscalculator/src/main/java/com/redhat/fuse/demos/ws/wssuma/SumProcessor.java
@@ -2,16 +2,23 @@ package com.redhat.fuse.demos.ws.wssuma;
 
 import org.apache.camel.Exchange;
 import org.apache.camel.Processor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class SumProcessor implements Processor {
 
+	private static final Logger logger = LoggerFactory.getLogger(SumProcessor.class);
+
 	@Override
 	public void process(Exchange exchange) throws Exception {
+		logger.info("Sum Request recieved: ");
 		SumDTO dto = exchange.getIn().getBody(SumDTO.class);
+		logger.debug("Message Recieved: "+dto.getOper1()+"+"+dto.getOper2());
 		int res = dto.getOper1()+dto.getOper2();
 		SumResult result = new SumResult();
 		result.setResult(res);
 		exchange.getIn().setBody(result);
+		logger.debug("Sum Request added to body");
 	}
 
 }
